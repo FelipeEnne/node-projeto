e8a796d8f6bd7a9b1c7c8cf5e6e6808d7d99b037
FelipeEnne <felipeenne@gmail.com>
2020-05-12 14:20:58 -0300

    connect to database

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 
 require('dotenv').config({path:'variable.env'})
 
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true , useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (error) => {
+    console.error('Error: '+ error.message);
+});
 
 app.set('port',process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
