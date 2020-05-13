e72c19ec964442b19b12d1e2b0f6a762f78d73ec
FelipeEnne <felipeenne@gmail.com>
2020-05-13 18:49:59 -0300

    add not Found page error404

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mustache  = require('mustache-express')
 const router = require('./routes/index')
 const helper = require('./helpers')
+const errorHandler = require('./handlers/erroHandler')
 
 
 const app = express();
@@ -16,6 +17,7 @@ app.use(express.json());
 
 app.use('/',router);
 
+app.use(errorHandler.notFound);
 
 app.engine('mst', mustache(__dirname+'/views/partials', '.mst'));
 app.set('view engine', 'mst');
