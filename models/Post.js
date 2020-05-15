ea62bce865faade9b9dbe5288b322ccb9b63b1eb
FelipeEnne <felipeenne@gmail.com>
2020-05-14 21:57:06 -0300

    create a model schema

---
+++ models/Post.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const postSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        trim:true,
+        required:"Precisa de titulo",
+    },
+    slug:String,
+    body:{
+        type:String,
+        trim:true,
+    },
+    tags:[String]
+});
