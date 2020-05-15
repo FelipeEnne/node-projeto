20d885d172fbc6d8a200238387e241c7419775cc
FelipeEnne <felipeenne@gmail.com>
2020-05-14 21:48:00 -0300

    remove the midware

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,13 +1,3 @@
-exports.userMiddleware = (req, res, next)=>{
-    let info = {
-        name:'Felipe',
-        id:123
-    }
-    req.userInfo = info;
-    next();
-}
-
-
 exports.index = (req, res)=>{
     let obj = {
         pageTitle:'HOME',
